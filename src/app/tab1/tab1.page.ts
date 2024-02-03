5da28c108631450c56061f45803aa8edb0221118
ccumaco <carloscumaco5@gmail.com>
2024-02-03 00:45:14 -0500

    adding content in 3 tabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
+  tasks: { name: string; description: string; completed: boolean }[] = [
+    { name: 'Task 1', description: 'Description for Task 1', completed: false },
+    { name: 'Task 2', description: 'Description for Task 2', completed: true },
+    { name: 'Task 3', description: 'Description for Task 3', completed: false },
+    { name: 'Task 4', description: 'Description for Task 4', completed: true },
+    { name: 'Task 5', description: 'Description for Task 5', completed: false },
+  ];
   constructor() {}
-
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  selectedTask: {
+    name: string;
+    description: string;
+    completed: boolean;
+  } = { name: '', description: '', completed: false };
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
 
-  constructor() {}
+  setResult(ev: any) {
+    console.log(`Dismissed with role: ${ev.detail.role}`);
+  }
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params) => {
+      if (params && params['task']) {
+        this.selectedTask = JSON.parse(params['task']);
+      }
+    });
+  }
 
+  saveChanges() {
+    console.log('Cambios guardados:', this.selectedTask);
+  }
 }
